09fd0b21a22a5041fc9b6fbf2a256942fa1d6ac6
Amber Lehua <46366315+amberlehua@users.noreply.github.com>
2021-09-19 18:42:30 -1000

    Add letter function

--- js/script.js
+++ js/script.js
@@ -6,5 +6,29 @@ const remaining = document.querySelector("span");
 const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again")
 
-//for debugging
+//for testing
 const word = "magnolia";
+    
+const update = function () {
+    const letters = word.split("");
+    const letterCount = [];
+    for (const key in letters) {
+        letterCount.push("●")
+    }
+    const totalLetters = letterCount.join("");
+    //console.log(totalLetters)
+    wordInProgress.innerHTML = totalLetters;
+    //for length of letterCount,letters.push("●");
+}
+
+update(word)
+
+guess.addEventListener("click", function (e) {
+    e.preventDefault();
+    //Create and name a variable to capture the value of the input. 
+    const capture = letter.value;
+    //Log out the value of the variable capturing the input. Then, empty the value of the input. 
+    console.log(capture);
+    letter.value = "";
+    //You should see the letter you enter into the input field in the console when the Guess button is clicked. 
+})
