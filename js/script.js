5ceaf769ee3ad189882ace66407e987c023e58c9
Amber Lehua <46366315+amberlehua@users.noreply.github.com>
2021-09-19 17:47:24 -1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLetter = document.querySelector(".guessed-letters");
+const guess = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector("span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again")
+
+//for debugging
+const word = "magnolia";
